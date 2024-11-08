3e3944282a6380e25493394e9055620563f9ca59
unknown <moelhosseny2020@gmail.com>
2024-11-08 23:53:22 +0200

    Modified All Files

--- JS/logic.js
+++ JS/logic.js
@@ -1,12 +1,53 @@
+let nav = document.querySelector("nav");
 let btn = document.querySelector(".btn");
-let navLinks = document.querySelector("nav");
-btn.addEventListener("click", () => {
-  btn.classList.toggle("burger");
-  navLinks.classList.toggle("active");
+let links = [
+  {
+    linkName: "Home",
+    href: "#home",
+  },
+  {
+    linkName: "Shop",
+    href: "#shop",
+  },
+  {
+    linkName: "About",
+    href: "#about",
+  },
+  {
+    linkName: "Services",
+    href: "#services",
+  },
+  {
+    linkName: "Contact",
+    href: "#contact",
+  },
+];
+let unOrderList = document.createElement("ul");
+links.forEach((link) => {
+  let anchor = document.createElement("a");
+  anchor.textContent = link.linkName;
+  anchor.href = link.href;
+  let li = document.createElement("li");
+  li.appendChild(anchor);
+  unOrderList.appendChild(li);
 });
+nav.appendChild(unOrderList);
+btn.addEventListener("click", () => {
+  btn.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
 document.addEventListener("click", (e) => {
-  if (!navLinks.contains(e.target) && !btn.contains(e.target)) {
-    navLinks.classList.remove("active");
-    btn.classList.remove("burger");
+  if (!nav.contains(e.target) && !btn.contains(e.target)) {
+    nav.classList.remove("active");
+    btn.classList.remove("active");
   }
 });
+
+let clickLinks = document.querySelectorAll("nav a");
+for (let i = 0; i < clickLinks.length; i++) {
+  clickLinks[i].addEventListener("click", () => {
+    nav.classList.remove("active");
+    btn.classList.remove("active");
+  });
+}
